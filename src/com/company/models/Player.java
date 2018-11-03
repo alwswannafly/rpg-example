049b5713eb51f5d11467a24fe780b67889c4f6ad
Владислав Марков <v.markov@kingbird.ru>
2018-11-04 00:36:22 +0400

    Create player model

---
+++ src/com/company/models/Player.java
@@ -0,0 +1,69 @@
+package com.company.models;
+
+public class Player {
+    private int health;
+    private int experience;
+    private int level;
+
+    private int skillPoints;
+    private int power;
+    private int skill;
+    private int intelligence;
+    
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getSkillPoints() {
+        return skillPoints;
+    }
+
+    public void setSkillPoints(int skillPoints) {
+        this.skillPoints = skillPoints;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getSkill() {
+        return skill;
+    }
+
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+}
